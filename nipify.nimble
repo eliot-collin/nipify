5c436143336064cc1d39326c5982444fa7df5362
eliot-collin <eliot.collin.xd@gmail.com>
2023-07-19 20:58:03 +0200

    better getIp handling

--- nipify.nimble
+++ nipify.nimble
@@ -1,6 +1,6 @@
 # Package
 
-version       = "0.1.0"
+version       = "0.2.0"
 author        = "eliot-collin"
 description   = "library for ipify.org"
 license       = "MIT"
--- src/nipify/ipify.nim
+++ src/nipify/ipify.nim
@@ -5,21 +5,11 @@ proc getCurrentIp(url: string): string =
   return response.body
 
 proc getIp*(isIpv6: bool, isJson: bool): string =
-  let ipifyUrl: string = "https://api.ipify.org"
+  var ipifyUrl =
+    if isIpv6: "https://api64.ipify.org"
+    else: "https://api.ipify.org"
 
-  if isIpv6 == true:
-    let ipifyUrl: string = "https://api64.ipify.org"
+  if isJson:
+    ipifyUrl &= "?format=json"
 
-    if isJson == true:
-      let ipifyFormatUrl = ipifyUrl & "?format=json"
-      return getCurrentIp(ipifyFormatUrl)
-
-    else:
-      return getCurrentIp(ipifyUrl)
-
-  if isJson == true:
-    let ipifyFormatUrl = ipifyUrl & "?format=json"
-    return getCurrentIp(ipifyFormatUrl)
-
-  else:
-    return getCurrentIp(ipifyUrl)
+  return getCurrentIp(ipifyUrl)
